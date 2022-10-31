7116361afa1273a27f3d1b5561fd07bad98d1880
Светлана Чернова <lana_19.87@mail.ru>
2022-10-31 15:58:02 +0600

    first commit

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,36 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void ShouldTakePostAnswer() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void ShouldCompareJsonPath() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+}

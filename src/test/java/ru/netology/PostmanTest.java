4bf674b78dfa193b6e4201a5d837c9e9077bb4a9
Светлана Чернова <lana_19.87@mail.ru>
2022-10-31 16:00:19 +0600

    falling test

--- src/test/java/ru/netology/PostmanTest.java
+++ src/test/java/ru/netology/PostmanTest.java
@@ -29,7 +29,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some some"))
         ;
     }
 
